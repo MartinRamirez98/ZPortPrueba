3ed4bab29e9aa3dd27333abe84918bed7480812a
Martin <martin.ramirez@pucp.pe>
2020-07-14 09:41:57 -0500

    Updates

---
+++ precache-manifest.f6b2192d950cdbc039cedf6435620c86.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737852846c55f900c5f2931664985dcb",
+    "url": "/ZPortPrueba/index.html"
+  },
+  {
+    "revision": "e2282a3adfb18378020d",
+    "url": "/ZPortPrueba/static/css/2.3ed17740.chunk.css"
+  },
+  {
+    "revision": "960ab46eff7ad0d0c091",
+    "url": "/ZPortPrueba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2282a3adfb18378020d",
+    "url": "/ZPortPrueba/static/js/2.5ef72f66.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/ZPortPrueba/static/js/2.5ef72f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960ab46eff7ad0d0c091",
+    "url": "/ZPortPrueba/static/js/main.f1c2b0a1.chunk.js"
+  },
+  {
+    "revision": "519002c649c15073bee7",
+    "url": "/ZPortPrueba/static/js/runtime-main.55f4a098.js"
+  },
+  {
+    "revision": "31155777442244768e6c9e54b2dbe851",
+    "url": "/ZPortPrueba/static/media/fondo.31155777.jpg"
+  },
+  {
+    "revision": "38c2abff674a46e0b243e2939b2246d1",
+    "url": "/ZPortPrueba/static/media/logo2.38c2abff.png"
+  },
+  {
+    "revision": "e5b867569043d84240038b6ddab2a7bd",
+    "url": "/ZPortPrueba/static/media/membresia.e5b86756.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/ZPortPrueba/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/ZPortPrueba/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/ZPortPrueba/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "2413e3b8a87c63e78059f0e9c1c4187d",
+    "url": "/ZPortPrueba/static/media/taller.2413e3b8.jpg"
+  }
+]);
